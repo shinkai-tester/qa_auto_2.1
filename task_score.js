2f8bc470bd5f5068cc2c9847cfe2701ada666f4c
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2024-04-20 01:48:57 +0200

    [hw 2_1_4] abbreviate first name with slice()

--- task_score.js
+++ task_score.js
@@ -2,11 +2,11 @@ let introduction = 90,
   git = 269,
   js = 120;
 let averageScore;
-let studentFirstName = "Sasha",
+let studentFirstName = "Aleksandra",
   studentLastName = "Klimantova";
 
 averageScore = (introduction + git + js) / 3;
 
 console.log(
-  `The average score of the student ${studentFirstName} ${studentLastName} is ${averageScore.toFixed(2)}`
+  `The average score of the student ${studentFirstName.slice(0, 1)}. ${studentLastName} is ${averageScore.toFixed(2)}`
 );
